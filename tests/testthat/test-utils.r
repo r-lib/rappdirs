97de816e7dc04f6d1da1704a76d63b44a5893d77
hadley <h.wickham@gmail.com>
2016-03-28 13:41:23 -0500

    Remove broken test
    Revealed by testthat 1.0.0

--- tests/testthat/test-utils.r
+++ tests/testthat/test-utils.r
@@ -2,7 +2,6 @@ context("utils")
 test_that("expand_r_libs_specifiers works as expected", {
   expect_equal(expand_r_libs_specifiers("%V"), as.character(getRversion()))
   expect_equal(expand_r_libs_specifiers("%%V"), "%V")
-  expect_output(expand_r_libs_specifiers("%p-platform/%v"), "/")
   expect_true(is.null(expand_r_libs_specifiers(NULL)))
 })
 test_that("parse_path_string works as expected", {
