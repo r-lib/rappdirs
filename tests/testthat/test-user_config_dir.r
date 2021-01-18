a11601cd787cda2e79034dabef9d2a90e22bd6c3
Hadley Wickham <h.wickham@gmail.com>
2021-01-18 12:47:28 -0600

    Get tests working on windows (#28)

--- tests/testthat/test-user_config_dir.r
+++ tests/testthat/test-user_config_dir.r
@@ -1,23 +1,35 @@
 test_that("user_config_dir works as expected", {
-    if (Sys.getenv("XDG_CONFIG_HOME", path.expand("~/.config")) == path.expand("~/.config")) {
-        expect_equal(user_config_dir("R", os="unix"), file_path("~/.config/R"))
-    }
-    expect_equal(user_config_dir("R", os="mac"), file_path("~/Library/Application Support/R"))
+    expect_equal(user_config_dir("R", os="mac"), "~/Library/Application Support/R")
     expect_equal(user_config_dir("R", version="%V", os="mac", expand=TRUE),
-                 file_path("~/Library/Application Support/R", as.character(getRversion())))
+                 file.path("~/Library/Application Support/R", as.character(getRversion())))
     expect_equal(user_config_dir("R", version="%V", os="mac", expand=FALSE),
-                 file_path("~/Library/Application Support/R/%V"))
-    if (!is.na(Sys.getenv("APPDATA", unset=NA))) {
-        expect_equal(user_config_dir("R", os="win", roaming=TRUE),
-                     file_path(Sys.getenv("APPDATA"), "R", "R"))
-    }
-    if (is.na(Sys.getenv("LOCALAPPDATA", unset=NA))) {
-        if (!is.na(Sys.getenv("USERPROFILE", unset=NA))) {
-            expect_equal(user_config_dir("R", os="win", roaming=FALSE),
-                     file_path(Sys.getenv("USERPROFILE"), "Local Settings", "Application Data", "R", "R"))
-        }
-    } else {
-        expect_equal(user_config_dir("R", os="win", roaming=FALSE),
-                     file_path(Sys.getenv("LOCALAPPDATA"), "R", "R"))
-    }
+                 "~/Library/Application Support/R/%V")
+})
+
+test_that("windows env simulation is correct", {
+    skip_on_os("windows")
+    withr::local_envvar(LOCALAPPDATA = NA, PROGRAMDATA = NA)
+
+    withr::local_envvar(APPDATA = "C:/config")
+    expect_equal(
+        user_config_dir("R", os="win", roaming=TRUE),
+        "C:/config/R/R"
+    )
+
+    withr::local_envvar("USERPROFILE" = "C:/config1")
+    expect_equal(
+        user_config_dir("R", os="win", roaming=FALSE),
+        "C:/config1/Local Settings/Application Data/R/R"
+    )
+
+    withr::local_envvar("LOCALAPPDATA" = "C:/config2")
+    expect_equal(
+        user_config_dir("R", os="win", roaming=FALSE),
+        "C:/config2/R/R"
+    )
+})
+
+test_that("on unix, uses XDG_CONFIG_HOME if set", {
+    withr::local_envvar(XDG_CONFIG_HOME = "/config")
+    expect_equal(user_config_dir("R", os="unix"), "/config/R")
 })
