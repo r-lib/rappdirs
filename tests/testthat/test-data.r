a336151f380338152fd86f64e748b40abcd32ae7
Hadley Wickham <h.wickham@gmail.com>
2021-01-18 12:51:39 -0600

    Rename test files

--- tests/testthat/test-data.r
+++ tests/testthat/test-data.r
@@ -1,3 +1,32 @@
+test_that("site_data_dir works as expected", {
+    expect_equal(site_data_dir("R", os="mac"), "/Library/Application Support/R")
+    expect_equal(site_data_dir("R", version="%V", os="mac", expand=TRUE),
+                 file.path("/Library/Application Support/R", as.character(getRversion())))
+    expect_equal(site_data_dir("R", version="%V", os="mac", expand=FALSE),
+                 "/Library/Application Support/R/%V")
+})
+
+test_that("can optionally use all XDG_DATA_DIRS", {
+    withr::local_envvar("XDG_DATA_DIRS" = "/usr/local/share:/usr/share")
+    expect_equal(
+        site_data_dir("R", os="unix", multipath = TRUE),
+        c("/usr/local/share/R", "/usr/share/R")
+    )
+    expect_equal(
+        site_data_dir("R", os="unix", multipath = FALSE),
+        "/usr/local/share/R"
+    )
+})
+
+
+test_that("site_config_dir works as expected", {
+    if (all("/etc/xdg" %in% parse_path_string(Sys.getenv("XDG_CONFIG_DIRS", "/etc/xdg")))) {
+        expect_equal(all("/etc/xdg/R" %in% site_config_dir("R", os="unix", multipath=TRUE)), TRUE)
+    }
+    expect_equal(site_config_dir("R", os="mac"), site_data_dir("R", os="mac"))
+})
+
+
 test_that("user_config_dir works as expected", {
     expect_equal(user_config_dir("R", os="mac"), "~/Library/Application Support/R")
     expect_equal(user_config_dir("R", version="%V", os="mac", expand=TRUE),
--- tests/testthat/test-site_config_dir.r
+++
@@ -1,7 +0,0 @@
-test_that("site_config_dir works as expected", {
-    if (all("/etc/xdg" %in% parse_path_string(Sys.getenv("XDG_CONFIG_DIRS", "/etc/xdg")))) {
-        expect_equal(all("/etc/xdg/R" %in% site_config_dir("R", os="unix", multipath=TRUE)), TRUE)
-    }
-    expect_equal(site_config_dir("R", os="mac"), site_data_dir("R", os="mac"))
-})
-
