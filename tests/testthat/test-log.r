d22f42d4bc83bcdd883624388919909b89ba6fc6
Hadley Wickham <h.wickham@gmail.com>
2021-01-18 13:17:45 -0600

    Update user_log_dir() tests

--- tests/testthat/test-log.r
+++ tests/testthat/test-log.r
@@ -1,7 +1,12 @@
 test_that("user_cache_dir works as expected", {
-   if (Sys.getenv("XDG_CACHE_HOME", path.expand("~/.cache")) == path.expand("~/.cache")) {
-        expect_equal(user_log_dir("R", os="unix"), "~/.cache/R/log")
-        expect_equal(user_log_dir("R", os="unix", opinion=FALSE), "~/.cache/R")
-    }
-    expect_equal(user_cache_dir("R", os="mac"), "~/Library/Caches/R")
+    expect_equal(user_log_dir("R", os="mac"), "~/Library/Logs/R")
+
+    withr::local_envvar(XDG_CACHE_HOME = "/cache")
+    expect_equal(user_log_dir("R", os="unix"), "/cache/R/log")
+    expect_equal(user_log_dir("R", os="unix", opinion=FALSE), "/cache/R")
+})
+
+test_that("user_cache_dir works with windows simulation", {
+    skip_on_os("windows")
+    expect_equal(user_log_dir("R", os = "win"), "<LOCALAPPDATA>/R/R/Logs")
 })
